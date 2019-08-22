877703e14e0d8bd6a33fb53f6dd8086b9cb02fcf
kuncup.harapan <kuncup.harapan@gmail.com>
2019-08-22 09:45:13 +0700

    Create informationboard_header.php

---
+++ application/views/templates/informationboard_header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Papan Informasi Hankam</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" type="image/png" href="<?php echo base_url() ?>images/home_iis_logo.png"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/custom.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/<?php echo $infoboard ?>.css"/>
+        <link rel="stylesheet" href="<?php echo base_url() ?>js/pg-calendar/dist/css/pignose.calendar.min.css" />
+        <script defer src="<?php echo base_url() ?>images/fontawesome/svg-with-js/js/all.js"></script>
+
+    </head>
+    <body>
+        <header class="d-flex bd-highligh border-bottom">
+            <div class="pr-0 pl-3 pt-2 flex-shrink-1 bd-highlight"><i class="fas fa-podcast fa-2x"></i></div>
+            <div class="p-0 w-100 bd-highlight"> <p class="text-center" style="font-weight: bold; font-size: 20pt">Media Informasi Pusat Teknologi Industri Pertahanan & Keamanan</p></div>
+        </header>
+
