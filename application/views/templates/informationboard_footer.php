63fa56be8942a7a3c0a59048cb82e1752477e0e4
kuncup.harapan <kuncup.harapan@gmail.com>
2019-08-22 11:01:07 +0700

    Update informationboard_footer.php

--- application/views/templates/informationboard_footer.php
+++ application/views/templates/informationboard_footer.php
@@ -8,33 +8,38 @@
 <script>window.jQuery || document.write('<script src="<?php echo base_url() ?>js/jquery-3.3.1.min.js"><\/script>')</script>
 <script type="text/javascript" src="<?php echo base_url() ?>bootstrap/js/bootstrap.bundle.js"></script>
 <script type="text/javascrip" src="<?php echo base_url() ?>js/jquery-ui/jquery-ui.min.js"></script>
-<script src="<?php echo base_url() ?>js/pg-calendar/dist/js/pignose.calendar.min.js"></script>
+<script src="<?php echo base_url() ?>js/pg-calendar/dist/js/pignose.calendar.full.js"></script>
 <script type="text/javascript">
-  $("#infoboardsidemenu").append("<div class=\"p-2 sidemenu active\" id=\"agenda\><a href=\"#\"><i class=\"far fa-calendar-alt fa-fw\"></i></a></div>");
-  $("#infoboardsidemenu").append("<div class=\"p-2 sidemenu \" id=\"agenda\><a href=\"#\"><i class=\"fas fa-bullhorn fa-fw\"></i></a></div>");
-  $("#infoboardsidemenu").append("<div class=\"p-2 sidemenu\" id=\"agenda\><a href=\"#\"><i class=\"far fa-newspaper fa-fw\"></i></a></div>");
-  $("div.sidemenu").on("mouseover", function(){
-      if($(this).hasClass("active")){
-          $(this).css({"background-color":"#FF8C6E"})
-      } else {
-          $(this).css({"background-color":"#FF8C6E"})
-          if($(this).hasClass("mouseleave")){
-              $(this).removeClass("mouseleave")
-          }
-          $(this).addClass("mouseover");
-      }
-  }).on("mouseleave", function(){
-      if($(this).hasClass("active")){
-          $(this).css({"background-color":"#F34213"})
-      } else {
-          $(this).css({"background-color":"#F34213"})
-          $(this).removeClass("mouseover");
-          $(this).addClass("mouseleave");
-      }
-      
-  }).on("click", function(){
-      $("div.sidemenu").removeClass("active mouseover");
-      $(this).addClass("active");
-  })
+    $(function () {
+        $('input.calendar').pignoseCalendar({
+            format: 'DD-MM-YYYY'
+        });
+    });
+    $("#infoboardsidemenu").append("<div class=\"p-2 sidemenu active\" id=\"agenda\><a href=\"#\"><i class=\"far fa-calendar-alt fa-fw\"></i></a></div>");
+    $("#infoboardsidemenu").append("<div class=\"p-2 sidemenu \" id=\"agenda\><a href=\"#\"><i class=\"fas fa-bullhorn fa-fw\"></i></a></div>");
+    $("#infoboardsidemenu").append("<div class=\"p-2 sidemenu\" id=\"agenda\><a href=\"#\"><i class=\"far fa-newspaper fa-fw\"></i></a></div>");
+    $("div.sidemenu").on("mouseover", function () {
+        if ($(this).hasClass("active")) {
+            $(this).css({"background-color": "#FF8C6E"})
+        } else {
+            $(this).css({"background-color": "#FF8C6E"})
+            if ($(this).hasClass("mouseleave")) {
+                $(this).removeClass("mouseleave")
+            }
+            $(this).addClass("mouseover");
+        }
+    }).on("mouseleave", function () {
+        if ($(this).hasClass("active")) {
+            $(this).css({"background-color": "#F34213"})
+        } else {
+            $(this).css({"background-color": "#F34213"})
+            $(this).removeClass("mouseover");
+            $(this).addClass("mouseleave");
+        }
+
+    }).on("click", function () {
+        $("div.sidemenu").removeClass("active mouseover");
+        $(this).addClass("active");
+    })
 </script>
 </html>
