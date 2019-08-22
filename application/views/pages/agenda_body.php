8f121a8c77a17dcd6c12510416070368d623b0b9
kuncup.harapan <kuncup.harapan@gmail.com>
2019-08-22 09:44:31 +0700

    Create agenda_body.php

---
+++ application/views/pages/agenda_body.php
@@ -0,0 +1,7 @@
+<main class="d-flex">
+    <div class="p-0 flex-shrink-1 bg-infoboard-1" style="font-size: 1.2em" id="infoboardsidemenu">
+    </div>
+    <div class="p-0 pl-2 pt-4 w-100 bd-highlight border-bottom">
+        Tes
+    </div>
+</main>
