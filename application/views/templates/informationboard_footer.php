e7a0ab9c3e2bd395b1a80b14859c868fb5084a6a
kuncup.harapan <kuncup.harapan@gmail.com>
2019-09-02 13:16:26 +0700

    Update informationboard_footer.php

--- application/views/templates/informationboard_footer.php
+++ application/views/templates/informationboard_footer.php
@@ -1,3 +1,23 @@
+<div class="modal fade" id="customizeModal" tabindex="-1" role="dialog" aria-labelledby="customizeModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="col-auto">
+                    <div class="bd-notif-icon d-flex justify-content-center align-items-center mr-1 border border-navy p-0 bg-navy rounded" style="min-width: 50px; max-height: 40px"><h5 class="pt-1 text-white text-uppercase" id="projectAbbr"><i class="fas fa-info-circle"></i></h5></div>
+                </div>
+                <h5 class="pt-2 mt-2" id="modalTitle"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="bg-eng-primary-1 text-white font-weight-bold text-monospace px-3 py-3 collapse" id="responseMessage">Sedang Memproses, Mohon Tunggu Sesaat ...</div>
+            <div class="modal-body" id="modalBody">
+            </div>
+            <div class="modal-footer" id="modalFooter">
+            </div>
+        </div>
+    </div>
+</div>
 <footer class="footer">
     <div class="d-flex justify-content-center p-2">
         <span class="svgfooterbg text-t1greenmed">Tim Pengembang SI-TI PTIPK &copy; 2018-2019</span>
@@ -40,6 +60,26 @@
     }).on("click", function () {
         $("div.sidemenu").removeClass("active mouseover");
         $(this).addClass("active");
-    })
+    });
+
+    $(document).ajaxStart(function () {
+        $("#modalBody").append("<div id=\"ajaxEvent\">Loading ...</div>");
+    });
+    $('#modalView').click(function () {
+        var agendaId = $(this).parent().attr('id');
+        $("#modalTitle").text("Informasi Agenda Kegiatan");
+        $("#modalBody").empty();
+        $("#modalFooter").empty();
+        /*
+        $.ajax({
+            url: "<?= site_url('information_board/index/1/1') ?>",
+            xhrFields: {
+                withCredentials: true
+            }
+        });
+        */
+        $("#modalBody").load("<?= site_url('information_board/index/1/') ?>"+ agendaId)
+        $('#customizeModal').modal('show')
+    });
 </script>
 </html>
