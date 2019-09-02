e1821a7f37282d6dfae9576bb54fa5781b248f40
kuncup.harapan <kuncup.harapan@gmail.com>
2019-09-02 13:16:17 +0700

    Update agenda_body.php

--- application/views/pages/agenda_body.php
+++ application/views/pages/agenda_body.php
@@ -2,67 +2,40 @@
     <div class="p-0 flex-shrink-1 bg-infoboard-1" style="font-size: 1.2em" id="infoboardsidemenu">
     </div>
     <div class="p-0 pl-2 pt-4 w-100 bd-highlight border-bottom">
-        <div class="d-flex justify-content-center"><input type="text" value="22-08-2019" id="text-calendar" class="calendar text-center" /></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="page-header">
-
-                    </div>
-                    <ul class="timeline">
-                        <li class="timeline-item">
-                            <div class="timeline-badge"><i class="glyphicon glyphicon-off"></i></div>
-                            <div class="timeline-panel">
-                                <div class="timeline-heading">
-                                    <h4 class="timeline-title">Kegiatan 1</h4>
-                                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> Serpong, jam 8.00 wib - 9.00 wib</small></p>
-                                </div>
-                                <div class="timeline-body">
-                                    <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-item">
-                            <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-                            <div class="timeline-panel">
-                                <div class="timeline-heading">
-                                    <h4 class="timeline-title">Kegiatan 2</h4>
-                                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> Thamrin, jam 8.30 wib - 9.00 wib</small></p>
-                                </div>
-                                <div class="timeline-body">
-                                    <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros gostis.</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-item">
-                            <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-                            <div class="timeline-panel">
-                                <div class="timeline-heading">
-                                    <h4 class="timeline-title">Kegiatan 3</h4>
-                                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> Serpong, 9.00 wib - 12.00 wib</small></p>
-                                </div>
-                                <div class="timeline-body">
-                                    <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                                    <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-
-                                </div>
-                            </div>
-                        </li>
-                        <li class="timeline-item">
-                            <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
-                            <div class="timeline-panel">
-                                <div class="timeline-heading">
-                                    <h4 class="timeline-title">Kegiatan 4</h4>
-                                    <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> Serpong, 13.00 wib - 15.00 wib</small></p>
-                                </div>
-                                <div class="timeline-body">
-                                    <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
+        <div class="d-flex justify-content-center"><input type="text" value="<?= date('d-m-Y')?>" id="text-calendar" class="calendar text-center" /></div>
+                    <?php
+                    $this->load->helper('text');
+                    if ($agenda != null) {
+                        $time = 6;
+                        foreach ($agenda as $key => $val) {
+                            $startts = strtotime($val['startdate'] . ' ' . $val['starttime']);
+                            $endts = strtotime($val['enddate'] . ' ' . $val['endtime']);
+                            $starttime = date('H', $startts);
+                            if ($time == 6) {
+                                echo '<div class="container mt-5 mb-5">';
+                                echo '<div class="row">';
+                                echo '<div class="col-9">';
+                                echo '<h4 class="pb-4">AGENDA HARI INI</h4>';
+                                echo '<ul class="timeline">';
+                            }
+                            echo '<li id="' . $val['id'] . '">'
+                            . '<a target="_blank" href="javascript:void(0)">' . strtoupper($val['title']) . '</a>'
+                            . '<a href="#" class="float-right">Pukul ' . $val['starttime'] . ' wib</a>'
+                            . '<p>' . word_limiter($val['desc'], 20, ' ...') . '</p>'
+                            . '<a href = "javascript:void(0);" class = "badge badge-dark" id = "modalView">BACA</a>'
+                            . '</li>';
+                            $time = $starttime;
+                        }
+                        echo '</ul>';
+                        echo '</div>';
+                        echo '</div>';
+                        echo '</div>';
+                    } else {
+                        echo 'tidak ada agenda kegiatan pada hari ini';
+                    }
+                    ?>
     </div>
 </main>
+<div class="fixed-bottom">
+    <p style="text-align: right;"><iframe src="https://www.jadwalsholat.org/adzan/ajax.row.php?id=24" frameborder="0" width="220" height="220"></iframe></p>
+</div>
