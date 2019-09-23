4914a09330a2f56e38180e2c5d25152389f58d16
kuncup.harapan <kuncup.harapan@gmail.com>
2019-09-23 09:51:09 +0700

    Update informationboard_header.php

--- application/views/templates/informationboard_header.php
+++ application/views/templates/informationboard_header.php
@@ -8,11 +8,14 @@
         <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/custom.css"/>
         <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/<?php echo $infoboard ?>.css"/>
         <link rel="stylesheet" href="<?php echo base_url() ?>js/pg-calendar/dist/css/pignose.calendar.min.css" />
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>bootstrap/css/bootstrap-datetimepicker.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>js/tagsinput/bootstrap-tagsinput.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>js/tagsinput/app.css"/>
         <script defer src="<?php echo base_url() ?>images/fontawesome/svg-with-js/js/all.js"></script>
 
     </head>
     <body>
-        <header class="d-flex bd-highligh border-bottom">
+        <header class="d-flex bd-highligh displayheader">
             <div class="pr-0 pl-3 pt-2 flex-shrink-1 bd-highlight"><i class="fas fa-podcast fa-2x"></i></div>
             <div class="p-0 w-100 bd-highlight"> <p class="text-center" style="font-weight: bold; font-size: 20pt">Media Informasi Pusat Teknologi Industri Pertahanan & Keamanan</p></div>
         </header>
