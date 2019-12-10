76508adfab7f76e85736081656eecbde17b89931
kuncup.harapan <kuncup.harapan@gmail.com>
2019-12-10 15:08:12 +0700

    Create Send_notification.php

---
+++ application/controllers/Send_notification.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Send_notification
+ *
+ * @author JOHNY
+ */
+class Send_notification extends CI_Controller {
+
+    //put your code here
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        // create curl resource
+        /*
+        $data = array('from' => array('type' => 'whatsapp', 'number' => '6288990422181'),
+            'to' => array('type' => 'whatsapp', 'number' => '6281328057709'),
+            'message' => array('content' => array('type' => 'text', 'text' => 'test message')));
+         * 
+         */
+
+    }
+
+}
