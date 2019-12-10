c7324b624cbaae7869c5f5d66528997123d80c1e
kuncup.harapan <kuncup.harapan@gmail.com>
2019-12-10 15:09:45 +0700

    Create mobview_header.php

---
+++ application/views/templates/mobview_header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Papan Informasi Hankam</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="icon" type="image/png" href="<?php echo base_url() ?>images/home_iis_logo.png"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/custom.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>css/<?php echo $infoboard ?>.css"/>
+        <link rel="stylesheet" href="<?php echo base_url() ?>js/pg-calendar/dist/css/pignose.calendar.min.css" />
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>bootstrap/css/bootstrap-datetimepicker.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>js/tagsinput/bootstrap-tagsinput.css"/>
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url() ?>js/tagsinput/app.css"/>
+        <script defer src="<?php echo base_url() ?>images/fontawesome/svg-with-js/js/all.js"></script>
+
+    </head>
+    <body>
+        <header class="d-flex bd-highligh displayheader">
+            <div class="pr-0 pl-3 pt-2 flex-shrink-1 bd-highlight"><a style="color: #000" href="javascript:void(0)"><i class="fas fa-podcast fa-2x"></i></a></div>
+            <div class="p-0 w-100 bd-highlight"> <span class="d-flex justify-content-center text-center pb-0 text-white-50" style="font-weight: bold; font-size: 1.5em">HANKAM INFOCON&trade;</span><span class="d-flex justify-content-center pb-1 pt-0 text-dark-50" style="font-size: 0.8em; font-weight: bolder">PTIPK Information & Communication Network</span></div>
+        </header>
+
