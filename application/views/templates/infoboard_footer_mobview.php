221c6a7ba7e93cf90a0f2559adc02b591e18322b
kuncup.harapan <kuncup.harapan@gmail.com>
2019-12-10 15:09:31 +0700

    Create infoboard_footer_mobview.php

---
+++ application/views/templates/infoboard_footer_mobview.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-2">
+    <div class="d-flex justify-content-center p-0">
+        <span class="text-dark" style="font-size: 0.8em">Proudly Presented by ICT Development Group - PTIPK &copy; 2018-2019</span>
+    </div>
+</footer>
